b8c96375d7813359bdf1d7b059a3bb48543f0382
Anmol Kapoor <anmolhome0227@gmail.com>
2020-05-28 16:02:28 -0700

    adding mobile functionality to the app

--- snake.js
+++ snake.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2020-05-27 14:04:26
+// Transcrypt'ed from Python, 2020-05-28 15:51:15
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {fabric} from './com.fabricjs.js';
 var __name__ = '__main__';
@@ -92,6 +92,23 @@ export var Snake =  __class__ ('Snake', [object], {
 		}
 		else if (e.keyCode == 80) {
 			alert ('Game Paused. Click OK to continue.');
+		}
+	});},
+	get turn_mobile () {return __get__ (this, function (self, event) {
+		console.log (event.clientX);
+		if (event.clientX < screen.width / 2) {
+			if (self.facing == 1) {
+				self.facing = 4;
+			}
+			else {
+				self.facing = facing [facing.index (self.facing) - 1];
+			}
+		}
+		else if (self.facing == 4) {
+			self.facing = 1;
+		}
+		else {
+			self.facing = facing [facing.index (self.facing) + 1];
 		}
 	});},
 	get die () {return __get__ (this, function (self) {
@@ -291,6 +308,9 @@ export var py_update = function () {
 		window.setTimeout (py_update, 500);
 	}
 };
+export var focus = function () {
+	var focus = !(focus);
+};
 export var pauseGame = function () {
 	alert ('Game Paused. Click OK to continue.');
 };
@@ -301,6 +321,7 @@ export var startTheGame = function () {
 	obstacle = Obstacle ();
 	mongoose = Mongoose ();
 	window.addEventListener ('keydown', player.turn, true);
+	document.addEventListener ('click', player.turn_mobile);
 	started = true;
 };
 py_update ();
